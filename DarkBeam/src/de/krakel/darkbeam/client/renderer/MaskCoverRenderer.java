d8fd1cbf7fad8bcd419e1703e8a3580841cc2716
krakel <uwe-git@doerl.de>
2013-06-24 13:02:14 +0200

    removed AStructureRenderer

--- DarkBeam/src/de/krakel/darkbeam/client/renderer/MaskCoverRenderer.java
+++ DarkBeam/src/de/krakel/darkbeam/client/renderer/MaskCoverRenderer.java
@@ -10,9 +10,10 @@ package de.krakel.darkbeam.client.renderer;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 
+import de.krakel.darkbeam.core.helper.LogHelper;
 import de.krakel.darkbeam.tile.TileStage;
 
-public class MaskCoverRenderer extends ACoverRenderer {
+public class MaskCoverRenderer extends AStructureRenderer {
 	public MaskCoverRenderer( int base) {
 		super( base);
 	}
@@ -27,5 +28,33 @@ public class MaskCoverRenderer extends ACoverRenderer {
 	public void renderSide( RenderBlocks rndrBlk, int area, Block blk, int meta, int x, int y, int z, TileStage tile) {
 		setSectionBounds( area, blk);
 		renderStandard( rndrBlk, blk, DIR_SOUTH, meta, x, y, z);
+	}
+
+	@Override
+	public void setSectionBounds( int area, Block blk) {
+		switch (area) {
+			case SIDE_DOWN:
+				blk.setBlockBounds( 0F, 0F, 0F, 1F, mSize, 1F);
+				break;
+			case SIDE_UP:
+				blk.setBlockBounds( 0F, 1F - mSize, 0F, 1F, 1F, 1F);
+				break;
+			case SIDE_NORTH:
+				blk.setBlockBounds( 0F, 0F, 0F, 1F, 1F, mSize);
+				break;
+			case SIDE_SOUTH:
+				blk.setBlockBounds( 0F, 0F, 1F - mSize, 1F, 1F, 1F);
+				break;
+			case SIDE_WEST:
+				blk.setBlockBounds( 0F, 0F, 0F, mSize, 1F, 1F);
+				break;
+			case SIDE_EAST:
+				blk.setBlockBounds( 1F - mSize, 0F, 0F, 1F, 1F, 1F);
+				break;
+			default:
+				LogHelper.warning( "unknown area %d", area);
+				blk.setBlockBounds( 0F, 0F, 0F, 1F, 1F, 1F);
+				break;
+		}
 	}
 }
